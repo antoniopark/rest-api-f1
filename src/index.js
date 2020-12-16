affb24aa8e08e595f0f4c95786c61cacea81a6c5
Rigoberto Antonio Reyes Gil <yamy-yugui@live.com.mx>
2020-12-16 15:14:59 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req,res)=> {
+    res.json({message: 'This is the F1'})
+})
+
+app.listen(app.get('port'));
+console.log('Server on port', app.get('port'));
